b55788ba69ebbf11d1f50eea481b80fd38041af3
Yannick Mauray <yannick.mauray@gmail.com>
2023-07-24 00:04:55 +0200

    feat: change action icon

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -24,7 +24,7 @@ class HomePage extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.sort),
+            icon: const Icon(Icons.play_arrow),
           ),
         ],
       ),
