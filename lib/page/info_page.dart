224e8fd3322d6deaceaecb2f72f07e575a9d8812
Yannick Mauray <yannick.mauray@gmail.com>
2023-07-28 20:53:22 +0200

    feat: basic info page

--- lib/page/info_page.dart
+++ lib/page/info_page.dart
@@ -1,6 +1,7 @@
 import 'package:chooose/widget/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class InfoPage extends ConsumerWidget {
   const InfoPage({super.key});
@@ -12,8 +13,47 @@ class InfoPage extends ConsumerWidget {
         title: const Text('Info'),
         actions: const [],
       ),
-      body: const Center(
-        child: Text('Info'),
+      body: Center(
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Image.asset('assets/img/choooose.png'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            FutureBuilder(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final packageInfo = snapshot.data;
+                  return ListTile(
+                    title: const Text('Version'),
+                    subtitle: Text(
+                      '${packageInfo!.version} (${packageInfo.buildNumber})',
+                    ),
+                  );
+                } else {
+                  return const ListTile(
+                    title: Text('Version'),
+                    subtitle: Text(
+                      'x.y.z (b)',
+                    ),
+                  );
+                }
+              },
+            ),
+            const ListTile(
+              title: Text('Author'),
+              subtitle: Text('Yannick Mauray'),
+            ),
+            const ListTile(
+              title: Text('License'),
+              subtitle: Text('LGPL-3.0'),
+            ),
+          ],
+        ),
       ),
     );
   }
