7500a3c21e9a413e4b337f9f5199cb910ced81f0
Yannick Mauray <yannick.mauray@gmail.com>
2023-08-03 12:54:51 +0200

    feat: RevenueCat init

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,24 @@ import 'dart:io';
 
 import 'package:chooose/app.dart';
 import 'package:chooose/firebase_options.dart';
+import 'package:chooose/revenue_cat_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Purchases.setLogLevel(LogLevel.debug);
+
+  if (Platform.isIOS) {
+    final configuration = PurchasesConfiguration(revenueCatApiKey);
+    await Purchases.configure(configuration);
+  }
 
   await SystemChrome.setEnabledSystemUIMode(
     SystemUiMode.manual,
